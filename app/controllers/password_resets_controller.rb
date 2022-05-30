80db4e07245064ab9b368495727c5e4ca2d5e48f
Anthony Jacquemin <an.jacquemin@gmail.com>
2022-05-30 11:49:04 +0200

    add password reset

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,28 +1,49 @@
 class PasswordResetsController < ApplicationController
   before_action :get_user, only: [:edit, :update]
   before_action :valid_user, only: [:edit, :update]
+  before_action :check_expiration, only: [:edit, :update]
 
 
   def new
   end
 
   def create
-    if (user = User.find_by(email: params[:password_reset][:email].downcase))
-      user.create_reset_digest
-      user.send_password_reset_email
+    if (@user = User.find_by(email: params[:password_reset][:email].downcase))
+      @user.create_reset_digest
+      @user.send_password_reset_email
       flash[:info] = "Email sent with password reset instructions"
       redirect_to root_url
     else
-      flash[:danger].now = "Email address not found"
+      flash.now[:danger] = "Email address not found"
       render 'new'
     end
   end
 
   def edit
-
   end
 
+  def update
+    if params[:user][:password].empty? # Case (3)
+      @user.errors.add(:password, "can't be empty")
+      render 'edit'
+    elsif @user.update(user_params) # Case (4)
+      log_in @user
+      @user.update_attribute(:reset_digest, nil)
+      flash[:success] = "Password has been reset."
+      redirect_to @user
+    else
+      render 'edit' # Case (2)
+    end
+  end
+
+
   private
+
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
+  # Before filters
 
   def get_user
     @user = User.find_by(email: params[:email])
@@ -35,5 +56,13 @@ class PasswordResetsController < ApplicationController
     end
   end
 
+  # Checks expiration of reset token.
+
+  def check_expiration
+    if @user.password_reset_expired?
+      flash[:danger] = "Password reset has expired."
+      redirect_to new_password_reset_url
+    end
+  end
 
 end
